6dbb690b91bce66e7a6e620c12483257b78a83dd
Fumihiro Ito <fmhrit@gmail.com>
2025-11-26 14:11:03 +0900

    example: Use gen_protobuf

---
+++ example/proto/github.com/cert-manager/cert-manager/apis/metav1/metav1_kubeproto.generated.object.go
@@ -0,0 +1,30 @@
+package metav1
+
+import (
+	"go.f110.dev/kubeproto/go/apis/metav1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+const GroupName = "cert-manager.io."
+
+var (
+	GroupVersion       = metav1.GroupVersion{Group: GroupName, Version: "v1"}
+	SchemeBuilder      = runtime.NewSchemeBuilder(addKnownTypes)
+	AddToScheme        = SchemeBuilder.AddToScheme
+	SchemaGroupVersion = schema.GroupVersion{Group: GroupName, Version: "v1"}
+)
+
+func addKnownTypes(scheme *runtime.Scheme) error {
+	scheme.AddKnownTypes(SchemaGroupVersion)
+	metav1.AddToGroupVersion(scheme, SchemaGroupVersion)
+	return nil
+}
+
+type ConditionStatus string
+
+const (
+	ConditionStatusTrue    ConditionStatus = "True"
+	ConditionStatusFalse   ConditionStatus = "False"
+	ConditionStatusUnknown ConditionStatus = "Unknown"
+)
